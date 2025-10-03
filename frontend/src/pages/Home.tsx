113e79a21f996e6a4bf345d0f4e2f2fa309f0553
Amanda Trinity <amandatrinity16@gmail.com>
2025-10-03 18:19:27 -0300

    feat(Home): cria o input para o usuário digitar

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,39 @@
+import '../styles/pages/home.css'
+import {useState} from 'react'
+
+
+function App() {
+  //Estado para guardar o valor que o usuário digitar
+  const [treino, setTreino] = useState('')
+
+  const handleEnviar = () => {
+    alert(`Seu treino de hoje é: ${treino}`)
+  }
+
+  return (
+      <div>
+
+        <h1>PersonalIA</h1>
+
+        <div className='card'>
+
+          <div className='text-card'>
+              {'Qual seu treino de hoje?'}
+          </div>
+
+        
+        <input
+          type='text'
+          placeholder='Digite aqui...'
+          value={treino}
+          onChange={(e) => setTreino(e.target.value)} //Atualiza o estado quando digita
+          className='input-treino'
+        />
+        
+        </div>
+
+      </div>
+  );
+}
+
+export default App
