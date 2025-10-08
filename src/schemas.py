8238ea26feaf97d9df9afe46d97d9458dab59c20
yasmws <ymws@cin.ufpe.br>
2025-10-08 01:39:37 -0300

    fix: ajusta a formatação do código no arquivo schemas.py

--- src/schemas.py
+++ src/schemas.py
@@ -1,9 +1,11 @@
-from pydantic import BaseModel
 from typing import Optional
+
+from pydantic import BaseModel
+
 
 class MensagemChat(BaseModel):
     mensagem_usuario: str
     nivel: Optional[str] = "iniciante"
     objetivo: Optional[str] = "condicionamento"
-    equipamentos: Optional[str] = "peso corporal" 
+    equipamentos: Optional[str] = "peso corporal"
     frequencia: Optional[str] = "2 dias por semana"
