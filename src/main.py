4562a961e84fb7a4fa872a00e42e40e2d312c15b
Amanda Trinity <amandatrinity16@gmail.com>
2025-10-14 20:05:35 -0300

    chore(main): configuração do cors

--- src/main.py
+++ src/main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
-
-app = FastAPI()
-
+from fastapi.middleware.cors import CORSMiddleware
 from routes.treino_routes import treino_router
+
+app = FastAPI(
+    title="PersonalIA API",
+    description="API para gerar treinos personalizados com IA.",
+    version="1.0.0"
+)
+
+#CONFIGURAÇÃO DO CORS
+
+# Lista de origens que podem fazer requisições para a API
+origins = [
+    "http://localhost:5173", # Endereço do frontend React/Vite
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, # Permite as origens da lista
+    allow_credentials=True,
+    allow_methods=["*"], # Permite todos os métodos (GET, POST, etc.)
+    allow_headers=["*"], # Permite todos os cabeçalhos
+)
 
 app.include_router(treino_router)
