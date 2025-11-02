c7512f1bb5b28781bc9d65c56db37e766824146f
arthursean <arthurscampos0@gmail.com>
2025-11-02 18:47:14 -0300

    feat: recovery-routes

--- src/routes/user_routes.py
+++ src/routes/user_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException
 
 from schemas import DadosUsr
-from services.sign_service import criar_usuario, ler_usuario
+from services.sign_service import criar_usuario, ler_usuario, mudar_senha, solicitar_recuperacao
 
 user_router = APIRouter(prefix="/usuario", tags=["Usr"])
 
@@ -24,3 +24,21 @@ async def post_usuario(data: DadosUsr):
 @user_router.get("/cadastro")
 async def get_usuario(email: str):
     return ler_usuario(email)
+
+
+@user_router.post("/solicitar-nova-senha")
+async def post_solicitacao(email: str):
+    try:
+        status, usr = solicitar_recuperacao(email)
+        return {"status": status, "usuario": usr}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@user_router.post("/mudar-senha")
+async def post_alterar(token: str, senha_nova: str):
+    try:
+        status = mudar_senha(token, senha_nova)
+        return {"status": status}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
