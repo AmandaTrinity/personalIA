5c30b342e835ab405f02a51bc3517ae246d83b3d
arthursean <arthurscampos0@gmail.com>
2025-11-09 18:30:50 -0300

    feat: add-email-service

--- src/services/email_service.py
+++ src/services/email_service.py
@@ -1,9 +1,11 @@
 import smtplib
+from config.settings import settings
 
 def enviar_email(email_usr: str, token: str):
     servico = smtplib.SMTP('smtp.gmail.com', 587)
     servico.starttls()
-    servico.login('personalia.sender@gmail.com', 'ewhhyeaesoecrmhm ')
-    envio = 'personalia.sender@gmail.com'
-    servico.sendmail(envio, email_usr, token)
+    email = settings.EMAIL
+    senha_smtp = settings.SENHA_SMTP
+    servico.login(email, senha_smtp)
+    servico.sendmail(email, email_usr, token)
     servico.quit()
