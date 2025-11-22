fcc3ac93f3b359288d92ad272a44c1be4281a980
Amanda Trinity <amandatrinity16@gmail.com>
2025-11-21 21:36:50 -0300

    styles(home): estrutura da área inicial da landing page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,29 +1,55 @@
-import { Link } from 'react-router-dom';    // Link é um container que vai levar a gente de uma página para outra
-import '../styles/pages/home.css'
+import Navbar from "../components/NavbarLanding";
+import "../styles/pages/home.css";
 
 function App() {
   return (
-      <div className='home-page-wrapper'>
-        <h1>PersonalIA</h1>
+    <div>
+      <Navbar/>
 
-        <div className='card'>  
-          <div className='text-card'>
-              {'Seu personal trainer de inteligência artificial.'}
+      <div className="container-inicial">
+        <div className="texto-inicial">
+          <div className="plano">Planos de treino personalizados com IA</div>
+          <h1>Seu Treino <br />Perfeito em Minutos</h1>
+          <p> Crie planos de treino personalizados baseados nos seus objetivos, 
+              experiência e preferências. Tudo de forma visual e intuitiva.</p>
+          <div className="botoes-iniciais">
+            <button className="botao-inicial">Começar Agora</button>
+            <button className="botao-saiba-mais">Saiba Mais</button>
           </div>
-
-        <p className='description-text'>
-          Descreva seus objetivos e criaremos o treino perfeito para você!
-        </p>        
-        <Link to="/chat">  
-          <button className='start-button'>
-            Começar Agora
-          </button>        
-        </Link> 
-
         </div>
 
+        <div className="container">
+          <div className="card-visual">
+            <div className="conteudo">
+              <div className="linha">
+                <div className="bolinha-ai">AI</div>
+                <div className="texto-falso">
+                  <div className="linha-grande"></div>
+                  <div className="linha-pequena"></div>
+                </div>
+              </div>
+
+              <div className="card-plano">
+                <div className="titulo-plano">Seu plano personalizado</div>
+                <div className="item-plano">
+                  <span>Segunda - Peito e Tríceps</span>
+                  <div className="indicador"></div>
+                </div>
+                <div className="item-plano">
+                  <span>Terça - Costas e Bíceps</span>
+                  <div className="indicador"></div>
+                </div>
+                <div className="item-plano">
+                  <span>Quarta - Pernas</span>
+                  <div className="indicador"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
   );
-}
+};
 
 export default App
